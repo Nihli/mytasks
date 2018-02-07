20638938b1daaad29cda5de9f5c662f448bce568
Nihli <lia.mellon17@gmail.com>
2018-02-07 21:46:23 +0000

    Modelo e migracoes

---
+++ db/migrate/20180207214230_create_tasks.rb
@@ -0,0 +1,12 @@
+# db/migrate/20180000000000_create_tasks.rb
+class CreateTasks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.boolean :done, default: false ########
+      t.references :list, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
